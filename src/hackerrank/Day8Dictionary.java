1bb0ef0a5ad2c0730d1cd772eab1c9c3f34f8453
J.T. Earl <jtearl188@gmail.com>
2016-05-05 18:45:31 -0400

    Final code for day 8 of the Hacker Rank 30 day challenge.

---
+++ src/hackerrank/Day8Dictionary.java
@@ -0,0 +1,30 @@
+package hackerrank;
+
+import java.util.*;
+import java.io.*;
+
+public class Day8Dictionary {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int phonebookSize = in.nextInt();
+		Map<String, Integer> phonebook = new HashMap<String, Integer>();
+		// Add contacts to phone book
+		for (int i = 0; i < phonebookSize; i++) {
+			String name = in.next();
+			int phone = in.nextInt();
+			// push contact to map
+			phonebook.put(name, phone);
+		}
+		while (in.hasNext()) {
+			String name = in.next();
+			if (phonebook.get(name) == null) {
+				System.out.println("Not found");
+			} else {
+				System.out.println(name + "=" + phonebook.get(name));
+			}
+		}
+		in.close();
+	}
+
+}
