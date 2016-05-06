b67f193a81b26922d906af58bec5c10b87e3ffac
J.T. Earl <jtearl188@gmail.com>
2016-05-06 14:37:31 -0400

    Completed factorial recursion method.

--- src/daysofcode/FactorialRecursion.java
+++ src/daysofcode/FactorialRecursion.java
@@ -1,10 +1,24 @@
 package daysofcode;
 
+import java.util.Scanner;
+
 public class FactorialRecursion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		if (number >= 2) {
+			System.out.println(factorial(number));
+		}
 
+	}
+
+	public static int factorial(int number) {
+		if (number <= 1) { // base case
+			return 1;
+		} else {
+			return number * factorial(number - 1);
+		}
 	}
 
 }
