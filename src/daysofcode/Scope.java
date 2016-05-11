e2eea5fbc3dadb9ad9f2b398294508ec016cfbef
J.T. Earl <jtearl188@gmail.com>
2016-05-11 14:36:16 -0400

    Added comment describing the task of the Scope program.

--- src/daysofcode/Scope.java
+++ src/daysofcode/Scope.java
@@ -1,5 +1,48 @@
 package daysofcode;
 
-public class Scope {
+import java.util.Scanner;
 
+//Complete the Difference class by writing the following:
+//A class constructor that takes an array of integers as a parameter and saves it to the elements instance variable.
+//A computeDifference method that finds the maximum absolute difference between any 2 numbers in N 
+//and stores it in the maxDifferencemaxDifference instance variable.
+
+public class Scope {
+	private int[] elements;
+	public int maximumDifference;
+
+	public Scope(int[] elements) {
+		this.elements = elements;
+	}
+
+	public int computeDifference() {
+		for (int i = 0; i < elements.length; i++) {
+			for (int j = 0; j < elements.length; j++) {
+				int temporaryMax = Math.abs(elements[i] - elements[j]);
+				if (temporaryMax > maximumDifference) {
+					maximumDifference = temporaryMax;
+				}
+			}
+		}
+		return maximumDifference;
+	}
+}
+
+// Hacker Rank premade solution class
+class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] a = new int[N];
+		for (int i = 0; i < N; i++) {
+			a[i] = sc.nextInt();
+		}
+
+		Scope D = new Scope(a);
+
+		D.computeDifference();
+
+		System.out.print(D.maximumDifference);
+	}
 }
