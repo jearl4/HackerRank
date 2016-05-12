58939754cfdd80e4aa9f1f4e39a3a21b7297b78a
J.T. Earl <jtearl188@gmail.com>
2016-05-11 20:21:04 -0400

    Added task comments to staircase algorithm code.

--- src/algorithms/Staircase.java
+++ src/algorithms/Staircase.java
@@ -1,7 +1,19 @@
 package algorithms;
 
 import java.util.Scanner;
-
+/**
+ * Print a staircase of height NN that consists of # symbols and spaces. 
+ * For example for N=6, here's a staircase of that height:
+ *      #
+ *     ##
+ *    ###
+ *   ####
+ *  #####
+ * ######
+ *
+ * @author J.T. Earl
+ *
+ */
 public class Staircase {
 
 	public static void main(String[] args) {
