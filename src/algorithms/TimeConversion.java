decfbdd13b37035db40026771a3dcfda19f14ed3
J.T. Earl <jtearl188@gmail.com>
2016-05-12 19:09:01 -0400

    Created TimeConversion class in algorithm package. Task of TimeConversion: 	Given a time in AM/PM format, convert it to military (24-hour) time.

---
+++ src/algorithms/TimeConversion.java
@@ -0,0 +1,11 @@
+package algorithms;
+
+import java.util.Scanner;
+
+public class TimeConversion {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String time = in.next();
+    }
+}
