ddb53d7abb4d6ba8823769591ab8f5a27f1cc03c
J.T. Earl <jtearl188@gmail.com>
2016-05-12 23:00:54 -0400

    Have AM times converted to military time.

--- src/algorithms/TimeConversion.java
+++ src/algorithms/TimeConversion.java
@@ -1,11 +1,25 @@
 package algorithms;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class TimeConversion {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String time = in.next();
-    }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		DecimalFormat leadingZeroFormatter = new DecimalFormat("00");
+		// String time = in.next();
+		String time = "07:05:45AM"; // Testing without Scanner
+
+		String amOrPm = time.substring(8); // is time AM or PM?
+		String seconds = time.substring(6, 8); // grab seconds
+		String minutes = time.substring(3, 5); // grab minutes
+		int hours = Integer.parseInt(time.substring(0, 2)); // grab hours and parse to integer
+		
+		if (amOrPm.equalsIgnoreCase("AM")) {
+			System.out.println(leadingZeroFormatter.format(hours) + ":" + minutes + ":" + seconds);
+		} else { // time is PM
+			System.out.println((hours + 12) + ":" + minutes + ":" + seconds);
+		}
+	}
 }
