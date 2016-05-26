698c43a7ddd7d4d339f7cf059a7b46e33c7fbbaa
J.T. Earl <jtearl188@gmail.com>
2016-05-26 12:41:14 -0400

    Added task comment

---
+++ src/daysofcode/PrimeNumbersRunningTime.java
@@ -0,0 +1,16 @@
+package daysofcode;
+
+/**
+ * A prime is a natural number greater than that has no positive divisors other
+ * than and itself. Given a number, n, determine and print whether it's prime or not prime.
+ * 
+ * Note: If possible, try to come up with a O(square root(n)) primality algorithm, or see what
+ * sort of optimizations you come up with for an O(n) algorithm. Be sure to check out
+ * the Editorial after submitting your code!
+ * 
+ * @author J.T. Earl
+ *
+ */
+public class PrimeNumbersRunningTime {
+
+}
