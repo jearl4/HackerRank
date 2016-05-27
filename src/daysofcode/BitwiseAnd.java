4fca4508edd02deb07d01a1d2187fb290d24e55d
J.T. Earl <jtearl188@gmail.com>
2016-05-26 23:27:12 -0400

    Added task comments

---
+++ src/daysofcode/BitwiseAnd.java
@@ -0,0 +1,14 @@
+package daysofcode;
+
+/**
+ * final day of 30 days of code! Given set S={1,2,3...,N}. Find two integers, A
+ * and B (where A < B ), from set S such that the value of A&B is the maximum
+ * possible and also less than a given integer, K. In this case, & represents
+ * the bitwise AND operator.
+ * 
+ * @author J.T. Earl
+ *
+ */
+public class BitwiseAnd {
+
+}
