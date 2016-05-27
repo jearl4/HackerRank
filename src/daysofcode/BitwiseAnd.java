1de612c3d3c9be5fd0bd07d9d668f877f6981506
J.T. Earl <jtearl188@gmail.com>
2016-05-26 23:28:21 -0400

    Added pre-built Hacker Rank code

--- src/daysofcode/BitwiseAnd.java
+++ src/daysofcode/BitwiseAnd.java
@@ -1,5 +1,7 @@
 package daysofcode;
 
+import java.util.Scanner;
+
 /**
  * final day of 30 days of code! Given set S={1,2,3...,N}. Find two integers, A
  * and B (where A < B ), from set S such that the value of A&B is the maximum
@@ -10,5 +12,12 @@ package daysofcode;
  *
  */
 public class BitwiseAnd {
-
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for (int a0 = 0; a0 < t; a0++) {
+			int n = in.nextInt();
+			int k = in.nextInt();
+		}
+	}
 }
