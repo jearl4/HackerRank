c988bc602631ee093425d729df279737ed1143cb
J.T. Earl <jtearl188@gmail.com>
2016-05-29 13:23:49 -0400

    Solved the pangram challenge

--- src/algorithms/Pangrams.java
+++ src/algorithms/Pangrams.java
@@ -1,23 +1,35 @@
 package algorithms;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Pangrams {
 
 	public static void main(String[] args) {
-		//Scanner in = new Scanner(System.in);
-		String input = "We promptly judged antique ivory buckles for the next prize";
-		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-		int j = 0;
-		int count = 0;
-		for (int i = 0; i < input.length(); i++) {
-			if (input.charAt(i) == alphabet.charAt(j)) {
-				count++;
-			}
+		// Scanner in = new Scanner(System.in);
+		String input = "We promptly judged antique ivory buckles for the prize";
+		Pangrams p = new Pangrams(input);
+		p.isPangram();
+	}
+
+	private final Set<Character> lettersNeeded = new HashSet<>();
+
+	public Pangrams(String s) {
+		for (char ch = 'a'; ch <= 'z'; ch++) {
+			lettersNeeded.add(ch);
 		}
-		if (count == 26) {
+		s = s.toLowerCase();
+		for (int i = 0; i < s.length(); i++) {
+			lettersNeeded.remove(s.charAt(i));
+		}
+	}
+	
+	public void isPangram(){
+		if(lettersNeeded.isEmpty()){
 			System.out.println("pangram");
-		} else {
+		}else{
 			System.out.println("not pangram");
 		}
 	}
