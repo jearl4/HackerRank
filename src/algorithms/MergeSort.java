b83448eed3a528f61e0f919f4d349e6d4c7e0844
J.T. Earl <jtearl188@gmail.com>
2016-06-07 18:17:13 -0400

    Sorts most cases but still fails in a worst case scenario A[4,3,2,1,0].

--- src/algorithms/MergeSort.java
+++ src/algorithms/MergeSort.java
@@ -1,5 +1,7 @@
 package algorithms;
 
+import java.util.Random;
+
 /**
  * Implementation of the merge sort in C.L.R.S. book. Takes O(n log n) time and
  * uses divide and conquer to sort an array.
@@ -11,39 +13,54 @@ package algorithms;
 public class MergeSort {
 
 	public static void main(String[] args) {
+		// Random rand = new Random();
+		int[] array = { 4, 3, 2, 1, 0 };
+		// fill array with random numbers 0-9
+		// for (int i = 0; i < array.length; i++) {
+		// array[i] = rand.nextInt(10);
+		// }
 
+		// sort array
+		mergeSort(array, 0, array.length);
+
+		// print sorted array
+		for (int j = 0; j < array.length; j++) {
+			System.out.print(array[j] + " ");
+		}
 	}
 
 	/**
-	 * Assumes that subarrays [p..q] and [q+1..r] are in sorted order. p, q, r
-	 * are indices into the array such that p <= q < r
+	 * Assumes that subarrays [leftPoint..midPoint] and [midPoint+1..rightPoint]
+	 * are in sorted order. leftPoint, midPoint, rightPoint are indices into the
+	 * array such that leftPoint <= midPoint < rightPoint
 	 * 
 	 * @param array
-	 * @param p
-	 * @param q
-	 * @param r
+	 * @param leftPoint
+	 * @param midPoint
+	 * @param rightPoint
 	 * @return
 	 */
-	private static int[] merge(int[] array, int p, int q, int r) {
-		int a = q - p + 1;
-		int b = r - q;
+	private static void merge(int[] array, int leftPoint, int midPoint, int rightPoint) {
+		int leftSize = midPoint - leftPoint;
+		int rightSize = rightPoint - midPoint;
 
-		int[] leftArray = new int[a + 1];
-		int[] rightArray = new int[b + 1];
+		// create left and right subarrays, add one to hold sentinel values
+		int[] leftArray = new int[leftSize + 1];
+		int[] rightArray = new int[rightSize + 1];
 
-		for (int i = 0; i < a; i++) {
-			leftArray[i] = array[p + i - 1];
-		}
+		// fill subarrays
+		for (int i = 0; i < leftSize; i++)
+			leftArray[i] = array[leftPoint + i];
+		for (int i = 0; i < rightSize; i++)
+			rightArray[i] = array[midPoint + i];
 
-		for (int i = 0; i < b; i++) {
-			rightArray[i] = array[q + i];
-		}
 		// assign sentinel values
-		leftArray[a + 1] = Integer.MAX_VALUE;
-		rightArray[b + 1] = Integer.MAX_VALUE;
+		leftArray[leftSize] = Integer.MAX_VALUE;
+		rightArray[rightSize] = Integer.MAX_VALUE;
 
-		int i = 1, j = 1;
-		for (int k = p; k < r; k++) {
+		// merge
+		int i = 0, j = 0;
+		for (int k = leftPoint; k < rightPoint; k++) {
 			if (leftArray[i] <= rightArray[j]) {
 				array[k] = leftArray[i];
 				i += 1;
@@ -52,5 +69,15 @@ public class MergeSort {
 				j += 1;
 			}
 		}
+	}
+
+	private static int[] mergeSort(int[] array, int left, int right) {
+		if (left < right) {
+			int middle = (int) Math.floor((left + right) / 2);
+			mergeSort(array, left, middle);
+			mergeSort(array, middle + 1, right);
+			merge(array, left, middle, right);
+		}
+		return array;
 	}
 }
