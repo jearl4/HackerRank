7bf1a6da8d591e90a3084266376428d3deebb1ac
J.T. Earl <jtearl188@gmail.com>
2016-06-07 13:21:55 -0400

    Task of class added. Merge sort implementation of pseudocode from C.L.R.S. book

---
+++ src/algorithms/MergeSort.java
@@ -0,0 +1,16 @@
+package algorithms;
+
+/**
+ * Implementation of the merge sort in C.L.R.S. book. Takes O(n) time and uses
+ * divide and conquer to sort an array.
+ * 
+ * @author J.T. Earl
+ *
+ */
+
+public class MergeSort {
+	
+	public static void main(String[] args) {
+
+	}
+}
