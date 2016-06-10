b1721c4bab4f95733eb8c219e4b03546b54032b2
J.T. Earl <jtearl188@gmail.com>
2016-06-10 15:28:49 -0400

    Added some comments at the start of the class

--- src/blackrocksprint/CurrencyArbitrage.java
+++ src/blackrocksprint/CurrencyArbitrage.java
@@ -2,15 +2,24 @@ package blackrocksprint;
 
 import java.util.Scanner;
 
+/**
+ * This code tries to convert $100,000 from USD to Euros to GBP back to USD for
+ * a profit. If there is no profit 0 is output. Part of the blackrock code
+ * sprint.
+ * 
+ * @author J.T. Earl
+ *
+ */
 public class CurrencyArbitrage {
 
 	public static void main(String[] args) {
+		// this will be used on Hacker Ranks web site to take in rates.
 		// Scanner in = new Scanner(System.in);
 		// int quotes = in.nextInt();
 		// double dollarToEuro = in.nextInt();
 		// double euroToGBP = in.nextInt();
 		// double gbpToUSD = in.nextInt();
-		
+
 		int quotes = 2;
 		double usdToEuro = 1.1837;
 		double euroToGBP = 1.3829;
