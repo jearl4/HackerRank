289b5334ad15636f7bef416e2dcaa7e3b40f012d
J.T. Earl <jtearl188@gmail.com>
2016-06-27 14:55:22 -0400

    Added task comments at the top of the class

--- src/weekOfCode21/Kangaroo.java
+++ src/weekOfCode21/Kangaroo.java
@@ -2,6 +2,17 @@ package weekOfCode21;
 
 import java.util.Scanner;
 
+/**
+ * There are two kangaroos on an x-axis ready to jump in the positive direction
+ * (i.e, toward positive infinity). The first kangaroo starts at location x1 and
+ * moves at a rate of v1 meters per jump. The second kangaroo starts at location x2
+ * and moves at a rate of v2 meters per jump. Given the starting locations and
+ * movement rates for each kangaroo, can you determine if they'll ever land at
+ * the same location at the same time?
+ * 
+ * @author J.T. Earl
+ *
+ */
 public class Kangaroo {
 	public static void main(String[] args) {
 		/*
