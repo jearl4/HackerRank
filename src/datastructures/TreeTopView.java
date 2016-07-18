61855fb2c2ba4974eef6891c67c61f08e2fefc2d
J.T. Earl <jtearl188@gmail.com>
2016-07-18 01:01:15 -0400

    Clarified code with comments.

--- src/datastructures/TreeTopView.java
+++ src/datastructures/TreeTopView.java
@@ -2,7 +2,15 @@ package datastructures;
 
 /**
  * You are given a pointer to the root of a binary tree. Print the top view of
- * the binary tree. You only have to complete the function.
+ * the binary tree. You only have to complete the function. 
+ *    3
+   /   \
+  5     2
+ / \   / \
+1   4 6   7
+ \       /
+  9     8
+ * the output would be 1 5 3 2 7 since from the top only these nodes would be visible.
  * 
  * @author J.T. Earl
  *
@@ -17,6 +25,12 @@ public class TreeTopView {
 		Node right;
 	}
 
+	/**
+	 * recursive function that handles calls topViewLeft, displays the root,
+	 * then calls topViewRight.
+	 * 
+	 * @param root
+	 */
 	@SuppressWarnings("unused")
 	private void top_view(Node root) {
 		if (root == null) {
@@ -28,6 +42,13 @@ public class TreeTopView {
 		}
 	}
 
+	/**
+	 * recursive function that moves from the bottom left of the BST up to the
+	 * top of the tree. Outputs only the left elements of the tree, the elements
+	 * that would be seen from a top down view of the tree.
+	 * 
+	 * @param root
+	 */
 	private void topViewLeft(Node root) {
 		if (root == null) {
 			return;
@@ -37,6 +58,13 @@ public class TreeTopView {
 		}
 	}
 
+	/**
+	 * recursive function that moves from the bottom right of the BST up to the
+	 * top of the tree. Outputs only the right elements of the tree, the
+	 * elements that would be seen from a top down view of the tree.
+	 * 
+	 * @param root
+	 */
 	private void topViewRight(Node root) {
 		if (root == null) {
 			return;
